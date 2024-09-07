5140ae0b2a563cd64cca70c32686681048c7c5f9
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-07 17:53:11 +0530

    Default files added

---
+++ src/test/java/com/edwin/chat/ChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
