be167671dde796df24264d3a8ee0fcbf18dc276f
edwinjoel1202 <edwinjoel1204@gmail.com>
2024-09-07 18:18:46 +0530

    Config created and 2 functions overrided from WebSocketMessageBrokerConfigurer

--- src/main/java/com/edwin/chat/ChatApplication.java
+++ src/main/java/com/edwin/chat/ChatApplication.java
@@ -1,4 +1,4 @@
-package com.edwin.chat;
+ package com.edwin.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/edwin/chat/config/WebSocketEventListener.java
@@ -0,0 +1,2 @@
+package com.edwin.chat.config;public class WebSocketEventListener {
+}
